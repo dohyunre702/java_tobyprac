d30fbe83d5cdcd69f2b5233114601e33d8556bd5
dohyu <dohyunre702@gmail.com>
2022-11-06 23:27:32 +0900

    connectionMaker 생성

---
+++ src/main/java/com/db/dao/NUserDao.java
@@ -0,0 +1,14 @@
+package com.db.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class NUserDao {
+    public Connection getConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection c = DriverManager.getConnection(
+                "jdbc:mysql://localhost/aaaa", user, password);
+        return c;
+    }
+}
--- src/main/java/com/db/dao/UserDao.java
+++ src/main/java/com/db/dao/UserDao.java
@@ -5,13 +5,19 @@ import com.db.domain.User;
 import java.sql.*;
 import java.util.Map;
 
-public abstract class Dao {
+public class UserDao {
+    private ConnectionMaker connectionMaker;
+
+    public UserDao() {
+        connectionMaker = new ConnectionMaker();
+    }
+
     public void add() throws ClassNotFoundException, SQLException {
         Map<String, String> env = System.getenv();
         String dbPassword = env.get("DB_PASSWORD");
 
         Class.forName("com.mysql.cj.jdbc.Driver");
-        Connection c = getConnection();
+        Connection c = connectionMaker.getConnection();
         PreparedStatement ps = c.prepareStatement(
                 "insert into users(id, name, password) values(?, ?, ?)");
         ps.setString(1, "01");
@@ -30,7 +36,7 @@ public abstract class Dao {
         String dbPassword = env.get("DB_PASSWORD");
 
         Class.forName("com.mysql.cj.jdbc.Driver");
-        Connection c = getConnection();
+        Connection c = connectionMaker.getConnection();
 
         PreparedStatement ps = c.prepareStatement(
                 "select * from users where id = ?");
@@ -47,11 +53,8 @@ public abstract class Dao {
         return user;
     }
 
-    //추상 메소드로 변형하기
-    public abstract Connection getConnection() throws ClassNotFoundException, SQLException;
-
     public static void main (String[]args) throws SQLException, ClassNotFoundException {
-        Dao dao = new Dao(); //error
-        dao.add();
+        UserDao userDao = new UserDao(); //error
+        userDao.add();
     }
 }
